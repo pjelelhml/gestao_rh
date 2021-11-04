09577b2a6e9d0f08755c7df74748b2ee4b3f745e
phml <paulo3385@hotmail.com>
2021-11-04 18:27:27 -0400

    deletando funcionarios

--- apps/funcionarios/urls.py
+++ apps/funcionarios/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from .views import FuncionariosList
+from .views import FuncionariosList, FuncionarioEdit, FuncionarioDelete
 
 urlpatterns = [
     path('', FuncionariosList.as_view(), name='list_funcionarios'),
+    path('edita/<int:pk>', FuncionarioEdit.as_view(), name='update_funcionario'),
+    path('delete/<int:pk>', FuncionarioDelete.as_view(), name='delete_funcionario'),
 
 ]
