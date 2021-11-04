d2d2a56aeef0cac355a05b698d0a23883ef88193
phml <paulo3385@hotmail.com>
2021-11-04 13:36:51 -0400

    crud empresa

--- gestao_rh/urls.py
+++ gestao_rh/urls.py
@@ -5,6 +5,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', include('apps.core.urls')),
     path('funcionarios/', include('apps.funcionarios.urls')),
+    path('empresa/', include('apps.empresas.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
 ]
