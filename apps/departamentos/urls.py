420bbf87cdbd389c663227cb57f9271832696c02
phml <paulo3385@hotmail.com>
2021-11-06 17:38:38 -0400

    terminado crud de departamentos

--- apps/departamentos/urls.py
+++ apps/departamentos/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 
-from .views import DepartamentosList
+from .views import (DepartamentosList,
+                    DepartamentoCreate,
+                    DepartamentoUpdate,
+                    DepartamentoDelete)
 urlpatterns = [
     path('list', DepartamentosList.as_view(), name='list_departamentos'),
+    path('create', DepartamentoCreate.as_view(), name='create_departamentos'),
+    path('update/<int:pk>', DepartamentoUpdate.as_view(), name='update_departamentos'),
+    path('delete/<int:pk>', DepartamentoDelete.as_view(), name='delete_departamentos'),
 ]
