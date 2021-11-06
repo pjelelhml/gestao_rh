e551a7ec080334070d064746eadb8de3e93c65b6
phml <paulo3385@hotmail.com>
2021-11-06 18:43:32 -0400

    crud documentos terminada

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -91,6 +91,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
@@ -100,3 +102,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'home'
 
 LOGOUT_REDIRECT_URL = 'login'
+
--- gestao_rh/urls.py
+++ gestao_rh/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
@@ -5,8 +7,9 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', include('apps.core.urls')),
     path('funcionarios/', include('apps.funcionarios.urls')),
-    path('departamentos/', include('apps.departamentos.urls')),
+    path('documentos/', include('apps.documentos.urls')),
+    path('departamen/', include('apps.departamentos.urls')),
     path('empresa/', include('apps.empresas.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
