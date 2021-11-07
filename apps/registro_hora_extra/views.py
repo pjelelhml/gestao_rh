1faca1eea48c460ef61f27c5b21026aa9bd40054
phml <paulo3385@hotmail.com>
2021-11-06 20:17:43 -0400

    crud registro hora extra completo

--- apps/registro_hora_extra/views.py
+++ apps/registro_hora_extra/views.py
@@ -1,4 +1,5 @@
-from django.views.generic import (ListView,)
+from django.urls import reverse_lazy
+from django.views.generic import (ListView, UpdateView, DeleteView, CreateView, )
 from .models import RegistroHoraExtra
 
 
@@ -8,3 +9,18 @@ class HoraExtraList(ListView):
     def get_queryset(self):
         empresa_logada = self.request.user.funcionario.empresa
         return RegistroHoraExtra.objects.filter(funcionario__empresa=empresa_logada)
+
+
+class HoraExtraEdit(UpdateView):
+    model = RegistroHoraExtra
+    fields = ['motivo', 'funcionario', 'horas']
+
+
+class HoraExtraDelete(DeleteView):
+    model = RegistroHoraExtra
+    success_url = reverse_lazy('list_hora_extra')
+
+
+class HoraExtraNovo(CreateView):
+    model = RegistroHoraExtra
+    fields = ['motivo', 'funcionario', 'horas']
