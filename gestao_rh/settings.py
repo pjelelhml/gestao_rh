ce94de6e734721edffcdafe7a309f6a735763c36
phml <paulo3385@hotmail.com>
2021-11-07 14:38:09 -0400

    subindo test

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -97,6 +97,8 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
+MEDIA_URL = '/media/'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
